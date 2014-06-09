98de8c00b1d9a85a797d4e4b7e19d28c96b0c154
Gregwar <g.passault@gmail.com>
2014-06-09 22:10:42 +0200

    Commenting kinematic.h (see #3)

--- firmware/src/kinematic.h
+++ firmware/src/kinematic.h
@@ -7,6 +7,34 @@
 #define RAD2DEG(a) ((a)*180.0/M_PI)
 #define DEG2RAD(a) ((a)*M_PI/180.0)
 
+/**
+ * Computes the inverse kinematics for one leg
+ *
+ * Input x, y and z are the targe cartesian position, 
+ * in the referencial of the leg. The z axis is upward,
+ * the x axis is alighed with the "0" angle of the first
+ * servo.
+ *
+ * Output are a, b and c, angles for the three servos,
+ * the first controls the yaw of the leg, through the
+ * motor fixed on the body, and b and c controls the
+ * length of the leg and the position of the end of 
+ * the leg.
+ *
+ * l1, l2 and l3 are the length between the first motor
+ * arm and the second motor arm, between the second and thr
+ * third motor arm, and between the third motor arm and the
+ * end of the leg.
+ *
+ *
+ *              b
+ *         l1 /   \ l2
+ *          /       c
+ *  body-a          | l3
+ *                  -
+ *                  (x, y, z) end of the leg
+ *
+ */
 bool computeIK(float x, float y, float z,
         float *a, float *b, float *c,
         float l1, float l2, float l3);
